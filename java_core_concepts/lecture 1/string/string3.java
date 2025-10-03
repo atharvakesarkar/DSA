aa58e3cda4ed6d7e85491a473302ca8fe5a60ec2
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-03 21:30:23 +0530

    Add files via upload
    it contain of string and string method

---
+++ java_core_concepts/lecture 1/string/string3.java
@@ -0,0 +1,48 @@
+
+class string3 {
+    public static void main(String[] args) {
+        
+    // Methods of string 
+    // concatenate
+    // String name1 = "Atharva";
+    // String name2 = "kesarkar";
+
+    // System.out.println(name1 + " "+ name2); //concatenate using add opertor
+
+
+
+
+    // Use of charAT for getting the char on define value
+
+    // String firstname = "Atharva";
+    // System.out.println(firstname.charAt(1)); 
+
+    // NOTE - in java you cannot give negative indexing...
+
+
+
+
+    // LENGTH - it use for getting length of string
+
+    // String name = "Atharva kesarkar";
+
+    // System.out.println(name.length()); //output - 16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
