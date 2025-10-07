75f78f702b45893959397c5015da63025d78bf8f
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-07 21:50:02 +0530

    added array code
    add array code , which consisit of array made in int , bool , string and also learn about the lenght property

---
+++ java_core_concepts/Array 4/array4.java
@@ -0,0 +1,50 @@
+public class array4 {   //all the program are in the class
+    public static void main(String[] args) {
+        
+
+        // IN JAVA, ARRAY ARE FIXED SIZE, MUTABLE, TYPE-SPECFIC CONTAINER THAT STORES MULTIPLE VALUE OF SAME DATA TYPE
+
+        // int[] student = new int[4];
+        // student[0] = 10;
+        // student[1] = 20;
+        // student[2] = 30;
+        // student[3] = 40;
+
+        // System.out.println(student[0]);
+
+        // IF A VARIABLE IS DECLARED BUT NOT INTIALIZED, IT AUTOMATICALLY GETS DEFAULT VALUE BASED ON  ITS TYPE 0 FOR NUMERIC TYPES. FALSE FOR BOOLEAN , AND NULL FOR STRING 
+
+        // String[] names = new String[5];
+
+        // System.out.println(names[0]); //AND NULL FOR STRING
+
+
+        // boolean[] truth = new boolean[6];
+
+        // System.out.println(truth[0]); // FALSE FOR BOOLEAN
+
+
+        // int[] num = new int[4];
+
+        // System.out.print(num[0]); //ZERO FOR INT
+
+
+
+
+        // ONE MORE PRATICE:-
+
+        // int[] num = new int[5];
+
+        // num[0] = 10;
+        // num[1] = 20;
+        // num[2] = 30;
+        // num[3] = 40;
+        // num[4] = 50;
+
+        // System.out.println(num[0]);
+
+ 
+
+    }
+}
+
---
+++ java_core_concepts/Array 4/methods_in_array/length_array_5.java
@@ -0,0 +1,16 @@
+public class length_array_5 {   //all the program are in the class
+    public static void main(String[] args) {
+
+        int[] num = new int[4];
+
+        num[0] = 10;
+        num[1] = 20;
+        num[2] = 30;
+        num[3] = 40;
+
+        System.out.println(num[3]);
+
+
+    }
+}
+
