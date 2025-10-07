508d5bdfdec457e2984c818ba11d3855ebf198f1
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-07 20:09:06 +0530

    Update string3.java

--- java_core_concepts/lecture 1/string/string3.java
+++ java_core_concepts/lecture 1/string/string3.java
@@ -45,16 +45,22 @@ class string3 {
     // SUBSTRING - This is use for printing specific part of string 
     // this start poisition is 0 and end postion is n-1
 
-    String name = "Atharva and Krutika";
+    // String name = "Atharva and Krutika";
 
-    System.out.println(name.substring(0,7));
+    // System.out.println(name.substring(0,7));
 
-    System.out.println(name); //string are always immutable 
-
-
+    // System.out.println(name); //string are always immutable 
 
 
 
+
+
+//  example..........
+    // String name = "Atharva kesarkar";
+
+    // String name2 = name.substring(8); //in this only start value is given so it will go till last 
+
+    // System.out.println(name2);
 
 
 
