059bfa3c5e7d6d1a854d901c9225384e72431481
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-08 21:09:10 +0530

    added the files of length and sort
    now i  have add the class file of length , cause it was deleted by me mistakely........ also added the sort file - which contain sort that java.util.Arrays.sort()

---
+++ java_core_concepts/Array 4/methods_in_array/sort_array6.java
@@ -0,0 +1,33 @@
+public class sort_array6 {   //all the program are in the class
+    public static void main(String[] args) {
+
+
+        // int[] num = new int[4];
+        // num[0] = 30;
+        // num[1] = 40;
+        // num[2] = 10;
+        // num[3] = 20;
+
+
+        // java.util.Arrays.sort(num); //java.util is a package in that Arrays is the class which contain sort method
+        // System.out.println(num[0]);
+
+
+        String[] students = {"Archie" , "Krutika", "Atharva", "Bhumika", "Aashi", "Kinmay"};
+        java.util.Arrays.sort(students);
+
+        // this is the sorted array
+        System.out.println(students[0]);
+        System.out.println(students[1]);
+        System.out.println(students[2]);
+        System.out.println(students[3]);
+        System.out.println(students[4]);
+        System.out.println(students[5]);
+
+
+
+        
+    
+    }
+}
+
