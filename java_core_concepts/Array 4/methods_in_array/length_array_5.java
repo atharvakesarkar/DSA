4d4ca30486420d2ef6f095e9799569c0e3ffc986
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-08 21:04:39 +0530

    just added the more code about array and method like length and sort

--- java_core_concepts/Array 4/methods_in_array/length_array_5.java
+++ java_core_concepts/Array 4/methods_in_array/length_array_5.java
@@ -1,16 +1,53 @@
+
+
 public class length_array_5 {   //all the program are in the class
     public static void main(String[] args) {
 
-        int[] num = new int[4];
+    
 
-        num[0] = 10;
-        num[1] = 20;
-        num[2] = 30;
-        num[3] = 40;
+        // int[] num = new int[4];
 
-        System.out.println(num[3]);
+        // num[0] = 20;
+        // num[1] = 10;
+        // num[2] = 30;
+        // num[3] = 40;
+
+        // System.out.println(num[3]);
+        // System.out.print(num.length); //using this length property we can easily get length of array
+
+        // System.out.println(num[0]);
+        // java.util.Arrays.sort(num); //this will sort the data in ascending order
+        // System.out.println(num[0]);
 
 
+        // int[] rollno = {24, 34, 20, 18, 1 , 90}; //we can make array like this, when we know about the elements
+        // System.out.print(rollno.length);
+
+
+
+        // String[] student = new String[5];
+        // student[0] = "Atharva";
+        // student[1] = "Krutika";
+        // student[2] = "Archie";
+        // student[3] = "Bhumika";
+        // student[4] = "Kinmay";
+
+
+        // java.util.Arrays.sort(student);
+        // System.out.println(student[0]);
+        // System.out.println(student[1]);
+        // System.out.println(student[2]);
+        // System.out.println(student[3]);
+        // System.out.println(student[4]);
+
+
+
+        // ANOTHER WAY TO CREATE ARRAY 
+
+        // int[] marks = {10,20,30,40,50,60};
+        // System.out.print(marks[0]);
+        
+    
     }
 }
 
