bf2751774980fcf86cc8d5333087d22af5ae4d95
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-09 23:27:20 +0530

    two dimension Array
    so this file includes the code related to the  two dimension array

---
+++ java_core_concepts/Array 4/methods_in_array/array_2d.java
@@ -0,0 +1,10 @@
+public class array_2d {
+    public  static void main (String[]  args){
+
+
+        int[] [] finalmarks = {{56,67,43} , {34,10,99}};
+
+        System.out.println(finalmarks[0][0]);
+
+    }
+}
