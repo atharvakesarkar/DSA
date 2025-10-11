42731f377330802d3342f5f01edede9e3da48ef3
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-11 18:23:22 +0530

    added one more code of string 2d array

--- java_core_concepts/Array 4/methods_in_array/array_2d.java
+++ java_core_concepts/Array 4/methods_in_array/array_2d.java
@@ -2,9 +2,16 @@ public class array_2d {
     public  static void main (String[]  args){
 
 
-        int[] [] finalmarks = {{56,67,43} , {34,10,99}};
+        // int[] [] finalmarks = {{56,67,43} , {34,10,99}};
 
-        System.out.println(finalmarks[0][0]);
+        // System.out.println(finalmarks[0][0]);
+        // System.out.println(finalmarks[0][1]);
+
+
+        // String [] [] student = {{"Atharva" , "Krutika"},{"Archie" , "soham"} , {"Bhumika", "pravina"}};
+
+
+        // System.out.println(student[2][1]);
 
     }
 }
