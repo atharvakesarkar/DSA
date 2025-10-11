24c493e1e221492bdc3da7862538a4c3de9afd8c
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2025-10-11 18:24:37 +0530

    includes the code of casting
    this folder contain the code of casting that is implict and explict

---
+++ java_core_concepts/casting 5/casting_5.java
@@ -0,0 +1,33 @@
+public class casting_5{
+    public static void main(String[]   args){
+
+
+        // System.out.println("hello world");
+        // casting -    
+
+        // IMPLICIT CASTING -  Automatic conversion of smaller data types into larger one by java. its safe and dosent lose data........
+        // double price = 100.00;
+        // double finalprice = price + 18;
+
+        // System.out.print(finalprice);
+
+
+
+
+    //      EXPLICIT CASTING - Manual conversion  of larger data type into the smaller one by the programmer.. if the memory of the smaller data type can't hold the larger value, some data may be lost 
+    
+        // int price = (int)100.00;
+        // int finalprice = price + 18;
+        
+        // System.out.print(finalprice);
+
+
+        byte number = (byte)129;
+
+        System.out.println(number);
+
+
+
+
+    }
+}
