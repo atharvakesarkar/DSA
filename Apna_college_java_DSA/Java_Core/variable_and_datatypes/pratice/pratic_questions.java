1813b5c14f53cb64f3f06f7383460ee936900f3e
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-02 23:35:14 +0530

    pratice question

---
+++ Apna_college_java_DSA/Java_Core/variable_and_datatypes/pratice/pratic_questions.java
@@ -0,0 +1,78 @@
+// import java.util.*;
+public class pratic_questions{ 
+    public static void main(String args[]){
+
+//q1.in a program, input 3 numbers: A,B,C. you have to output the average of these three numbers 
+        
+        //user input - creared a object name as sn1 , sn2 , sn3
+        //defined the variable and integrated user input values 
+    //     Scanner sn1 = new Scanner(System.in); 
+    //     System.out.println("enter a num1 value: ");
+    //     int num1 = (int) sn1.nextInt();
+
+    //     Scanner sn2 = new Scanner(System.in); 
+    //     System.out.println("enter a num2 value: ");
+    //     int num2 = (int) sn2.nextInt();
+
+    //     Scanner sn3 = new Scanner(System.in); 
+    //     System.out.println("enter a num3 value: ");
+    //     int num3 = (int) sn3.nextInt();
+   
+    // //_____________________________________________________________________________________
+    //     // calucalting average value 
+    //     double soln = num1 + num2 + num3/ 3;
+
+    //     System.out.println("average of all the three values are: " + soln);
+
+
+//---------------------------------------------------------------------------------------------------------------
+//q2.in a program, input side of square. you have the output  the area of the square 
+
+        //user input:
+    //     Scanner sn = new Scanner(System.in);
+    //     System.out.print("enter the size of square side:  ");
+    //     int side = sn.nextInt();
+
+    // //_____________________________________________________________________________________________
+    //     //define solu variable for applying area of square forumla 
+    //     float solu = side * side;
+    //     System.out.println("The area of square is: "+solu);
+
+//---------------------------------------------------------------------------------------------------------------
+//q3. enter cost of 3 items from the user(using the float data type) - a pencil , a pen and eraser. you have to output the total cost of the items back to the user as their bill 
+
+//add no :  you can also trying adding 18% gst tax to the items in the bill as an a advanced problem 
+
+        //user input for pen , pencil and eraser
+
+    
+        // //created a object for user input 
+        // Scanner scan1 = new Scanner(System.in);
+
+
+        // //integrated object in the variable
+        // System.out.println("enter the cost of pen: ");
+        // float pen = scan1.nextFloat();
+      
+        // System.out.println("enter the cost of pencil: ");
+        // float pencil = scan1.nextFloat();
+
+        // System.out.println("enter the cost of eraser: ");
+        // float eraser = scan1.nextFloat();
+
+    
+
+        // float solution = (pen + pencil + eraser);
+        // System.out.println("total ammout "+solution);
+        // System.out.println("total ammout with GST "+solution * 1.18); //added gst cost
+        
+
+
+
+
+
+
+
+
+    }
+}
