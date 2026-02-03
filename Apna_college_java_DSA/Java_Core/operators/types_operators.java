360390148c4716cd7bbb54fc3f23ea0d19ee3137
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-03 23:44:28 +0530

    Add files via upload

---
+++ Apna_college_java_DSA/Java_Core/operators/types_operators.java
@@ -0,0 +1,51 @@
+public class types_operators{
+    public static void main(String args[]){
+
+        //type of opertors 
+        // 1.Arthimetic opertor => + , - , * , / , % , ++ , --
+        // 2.relational opertor  => -= , != , > , < , >=, <=
+        // 3. logical opertor => && , || , !
+        //5.Assignment opertor => = , += , -= , /= , *=
+
+//----------------------------------------------------------------------------------------------------------------
+//example of unary pre and post opertor (increment and decrement) 
+        // int num1 = 10;
+
+        // ++num1; //added one value and now the output will be 12
+        // System.out.println(num1);
+
+        // int num2 = 12;
+        // System.out.println(num2); //before increment value print is 12 
+        // num2++;
+        // System.out.println(num2); //after increment value print is 13
+
+//-------------------------------------------------------------------------------------------------------------
+//logical opeartor 
+
+        // boolean and = 10>5 && 5 == 5;
+        // System.out.println(and); //both condition should be true 
+
+        // boolean or = 10<5 || 5 == 5;
+        // System.out.println(or); //any one condition should be true
+
+        // boolean not =   !(3>2);
+        // System.out.println(not); //not opertor will make every true to false and false to true..inshort it will make ulta
+
+//----------------------------------------------------------------------------------------------------------------
+//Assignment opertor 
+
+        int a = 10;
+        a += 10; //already the value of a is 10 and by using += we have added more 10 into the a variable 
+
+        System.out.println(a); 
+
+
+
+
+
+
+
+
+
+    }
+}
