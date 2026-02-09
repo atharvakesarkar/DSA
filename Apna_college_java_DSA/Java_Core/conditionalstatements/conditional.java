5f54442c4dc4cf65a949a92c851f1b887bb0f6be
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-09 21:22:52 +0530

    added the code of else if
    solve the questions like income tax caluclator and print the largest of three number

--- Apna_college_java_DSA/Java_Core/conditionalstatements/conditional.java
+++ Apna_college_java_DSA/Java_Core/conditionalstatements/conditional.java
@@ -1,11 +1,77 @@
+// import java.util.*;
+
 public class conditional{
     public static void main(String args[]){
         
-        int age = 10;
-        if (age == 10){
-            System.out.println("you are not elgible for the licencse");
-        }
+        // int age = 10;
+        // if (age == 10){
+        //     System.out.println("you are not elgible for the licencse");
+        // }
 
+//-----------------------------------------------------------------------------------------------------------------
+    //printing largest number of 2
+        // int a = 10 , b = 20;
+
+        // if (a > b){
+        //     System.out.println("a is greater than b");
+        // } else {
+        //     System.out.println("b is greater than a ");
+        // }
+
+//-----------------------------------------------------------------------------------------------------------------
+
+    //print if the number is odd or even
+
+    // Scanner scan = new Scanner(System.in);
+    // System.out.print("enter a number: ");
+    // int num = scan.nextInt();
+
+    // if (num % 2 == 0){
+    //     System.out.println(num + " is even number");
+    // } else {
+    //     System.out.println(num + " is odd number");
+    // }
+
+//-----------------------------------------------------------------------------------------------------------------
+    // else if condition is use to check the multiple condition 
+
+    // int age = 14;
+
+    // if (age>= 18){
+    //     System.out.println("you are adult");
+    // } else if (age>= 13 && age<18) {
+    //     System.out.println("you are teenager");
+
+    // } else {
+    //     System.out.println("you are child");
+    // }
+
+//-----------------------------------------------------------------------------------------------------------------
+    // income tax calulator:
+
+    // System.out.print("enter your income: ");
+    // int income = scan.nextInt();
+
+    // if (income < 500000){
+    //     System.out.println("0% tax applied");
+    // } else if (income>= 500000 && income<1000000){
+    //     System.out.println(income * (0.2) + " 20% tax applied");
+    // } else {
+    //     System.out.print(income * (0.3) + " 30% tax applied");
+    // }
+
+//-----------------------------------------------------------------------------------------------------------------
+    //print the largest of three number 
+
+    // int a = 1 , b = 3 , c = 6;
+    
+    // if (a >= b && a >=c){
+    //     System.out.println("a is the largest number");
+    // } else if (b >= a && b >=c){
+    //     System.out.println("b is the largest number");
+    // } else {
+    //     System.out.println("c is the largest number");
+    // }
 
 
     }
