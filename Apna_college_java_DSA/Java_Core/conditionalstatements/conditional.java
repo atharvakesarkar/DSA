084236bcd662809242b1e998dde7039133dd64b7
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-09 06:25:42 +0530

    if else statment
    just added a basic if else code

---
+++ Apna_college_java_DSA/Java_Core/conditionalstatements/conditional.java
@@ -0,0 +1,12 @@
+public class conditional{
+    public static void main(String args[]){
+        
+        int age = 10;
+        if (age == 10){
+            System.out.println("you are not elgible for the licencse");
+        }
+
+
+
+    }
+}
