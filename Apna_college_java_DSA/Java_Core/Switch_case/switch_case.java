ab567cf298247e161cdb58b99402e8e473f51ce9
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-17 17:48:29 +0530

    Update switch_case.java
    solve the pratice questions given by apna college

--- Apna_college_java_DSA/Java_Core/Switch_case/switch_case.java
+++ Apna_college_java_DSA/Java_Core/Switch_case/switch_case.java
@@ -1,10 +1,11 @@
-import java.util.*;
-public class switch_case {
-    public static void main(String args[]){
 
+import java.util.*;
+
+public class switch_case {
+
+    public static void main(String args[]) {
 
         // int num = 1;
-
         // switch (num) {
         //     case 1 : System.out.println("samosa");
         //               break;
@@ -14,35 +15,88 @@ public class switch_case {
         //               break;
         //     default: System.out.println("wake up sid");
         // }
-
-
 //---------------------------------------------------------------------------------------------------------------
 // make the caluclator using switch case
-
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("enter your first number: ");
-        int a = scan.nextInt();
-        System.out.print("enter your second number: ");
-        int b = scan.nextInt();
-        System.out.print("enter your opertor: ");
-        char operator = scan.next().charAt(0);
-
-        switch (operator){
-            case '+' : System.out.println("addition of a and b will be: " + (a+b));
-                        break;
-            case '-' : System.out.println("subtraction of a and b will be: " + (a-b));
-                        break;
-            case '*' : System.out.println("Multiplication of a and b will be: " + (a*b));
-                        break;
-            case '/' : System.out.println("divide of a and b will be: " + (a/b));
-                        break;
-            case '%' : System.out.println("Modolus of a and b will be: " + (a%b));
-                        break;
-            default: System.out.println("error: try again");
-        }
-
-
-
+        // System.out.print("enter your first number: ");
+        // int a = scan.nextInt();
+        // System.out.print("enter your second number: ");
+        // int b = scan.nextInt();
+        // System.out.print("enter your opertor: ");
+        // char operator = scan.next().charAt(0);
+        // switch (operator){
+        //     case '+' : System.out.println("addition of a and b will be: " + (a+b));
+        //                 break;
+        //     case '-' : System.out.println("subtraction of a and b will be: " + (a-b));
+        //                 break;
+        //     case '*' : System.out.println("Multiplication of a and b will be: " + (a*b));
+        //                 break;
+        //     case '/' : System.out.println("divide of a and b will be: " + (a/b));
+        //                 break;
+        //     case '%' : System.out.println("Modolus of a and b will be: " + (a%b));
+        //                 break;
+        //     default: System.out.println("error: try again");
+        // }
+//-----------------------------------------------------------------------------------------------------------------
+// pratice questions
+        // q1. write a java program to get a number from the user and print wheter the number it is postive or negative 
+        // int num = scan.nextInt();
+        // if (num < 0){
+        //     System.out.println("negative number");
+        // } else if  (num >= 0) {
+        //     System.out.println("positive number");
+        // } else {
+        //     System.out.println("wrong input");
+        // }
+        //_____________________________________________________________________________________________________________________\
+        //q2. finish the following code so that it prints you have fever if your temprature is about 100 and otherwise prints you dont have fever 
+        // double temp =103.5;
+        // if (temp >= 100){
+        //     System.out.println("you have fever");
+        // } else {
+        //     System.out.println("you dont have fever");
+        // }
+        //____________________________________________________________________________________________________________________
+        //q3. write a java program to input week days (1-7) and print day of week name using switch case 
+        // System.out.print("enter weekday number: ");
+        // int weekday = scan.nextInt();
+        // switch (weekday){
+        //     case 1 : System.out.println("Sunday");
+        //     break;
+        //     case 2 : System.out.println("Monday");
+        //     break;
+        //     case 3 : System.out.println("Tuesday");
+        //     break;
+        //     case 4 : System.out.println("Wednesday");
+        //     break;
+        //     case 5 : System.out.println("Thrusday");
+        //     break;
+        //     case 6 : System.out.println("Friday");
+        //     break;
+        //     case 7 : System.out.println("Saturday");
+        //     break;
+        //     default: System.out.println("Wrong input");
+        // }
+        //_______________________________________________________________________________________________________________________
+        //q4. what will be the value of x and y in the following program
+        // int a = 63, b = 36;
+        // boolean x  = (a < b)? true : false;
+        // int y = (a>b)? a : b; 
+        // System.out.println(x);
+        // System.out.println(y);
+        //_______________________________________________________________________________________________________________________
+        //q5. write a java program  that takes a year from the user and print wheter that year is leap year or not 
+        // System.out.print("enter year: ");
+        // double year = scan.nextDouble();
+        // if (year % 400 == 0) {
+        //     System.out.println("It is a leap year");
+        // } else if (year % 100 == 0) {
+        //     System.out.println("It is not a leap year");
+        // } else if (year % 4 == 0) {
+        //     System.out.println("It is a leap year");
+        // } else {
+        //     System.out.println("It is not a leap year");
+        // }
     }
 }
