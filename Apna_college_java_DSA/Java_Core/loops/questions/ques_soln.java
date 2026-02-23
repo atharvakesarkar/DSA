691b60c5df8a46f3a71c3d1f7cc9d5acbd9634b1
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-23 21:15:30 +0530

    added the code for break and continue

---
+++ Apna_college_java_DSA/Java_Core/loops/questions/ques_soln.java
@@ -0,0 +1,20 @@
+
+// import java.util.*;
+
+// public class ques_soln {
+
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         while (true) {
+//             System.out.print("enter a number: ");
+//             int num = sc.nextInt();
+
+//             if (num / 1 == num) {
+//                 break;
+//             }
+//         }
+
+//     }
+// }
