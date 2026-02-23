231e25f13686a6fef3397de1ae8e215acc2606d5
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-23 21:14:13 +0530

    Comment out original loop and add user input structure
    Commented out the original loop and added a new structure for user input handling.

--- Apna_college_java_DSA/Java_Core/loops/breakcontinue/breakeg.java
+++ Apna_college_java_DSA/Java_Core/loops/breakcontinue/breakeg.java
@@ -1,14 +1,25 @@
 
+// import java.util.*;
 public class breakeg {
 
     public static void main(String args[]) {
 
-        for (int i = 1; i <= 5; i++) {
-            if (i == 3) {
-                break;
-            }
-            System.out.println(i);
-        }
-
+        // for (int i = 1; i <= 5; i++) {
+        //     if (i == 3) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+//----------------------------------------------------------------------------------------------------
+        //keep entering numbers till user enters a multiple 10 
+        // Scanner sc = new Scanner(System.in);
+        // while (true) {
+        //     int num = sc.nextInt();
+        //     if (num % 10 == 0) {
+        //         break;
+        //     }
+        //     System.out.println(num);
+        // }
+        // System.out.println("loop has ended");
     }
 }
