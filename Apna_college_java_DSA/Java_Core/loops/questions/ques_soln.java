091948cb8976105f34c8856ea68369a18b5f4ad7
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-24 20:07:51 +0530

    add the code
    this code is about - check if the number is prime or not

--- Apna_college_java_DSA/Java_Core/loops/questions/ques_soln.java
+++ Apna_college_java_DSA/Java_Core/loops/questions/ques_soln.java
@@ -1,20 +1,31 @@
+import java.util.*;
 
-// import java.util.*;
+public class ques_soln {
 
-// public class ques_soln {
+    public static void main(String[] args) {
 
-//     public static void main(String[] args) {
+//q1. check if the number is print or not
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
 
-//         Scanner sc = new Scanner(System.in);
+        boolean isprime = true;
+        if (n == 2) {
+            System.out.println("n is the prime number");
+        } else {
+            for (int i = 2; i < n - 1; i++) {
+                if (n % 1 == 0) {
+                    isprime = false;
 
-//         while (true) {
-//             System.out.print("enter a number: ");
-//             int num = sc.nextInt();
+                }
+            }
+            if (isprime == true) {
+                System.out.println("n is a prime number");
+            } else {
+                System.out.println("n is not a prime number");
+            }
 
-//             if (num / 1 == num) {
-//                 break;
-//             }
-//         }
+        }
 
-//     }
-// }
+    }
+}
