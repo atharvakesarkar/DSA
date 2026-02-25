a1348fe9df2646912efe81ea2f5f525f1fe5a1eb
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-02-25 18:23:11 +0530

    solve mutliple questions

--- Apna_college_java_DSA/Java_Core/loops/questions/ques_soln.java
+++ Apna_college_java_DSA/Java_Core/loops/questions/ques_soln.java
@@ -1,31 +1,69 @@
+
 import java.util.*;
 
 public class ques_soln {
 
     public static void main(String[] args) {
 
-//q1. check if the number is print or not
+// //q1. check if the number is print or not
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter a number: ");
-        int n = sc.nextInt();
+//         System.out.print("enter a number: ");
+//         int n = sc.nextInt();
 
-        boolean isprime = true;
-        if (n == 2) {
-            System.out.println("n is the prime number");
-        } else {
-            for (int i = 2; i < n - 1; i++) {
-                if (n % 1 == 0) {
-                    isprime = false;
+//         boolean isprime = true;
+//         if (n == 2) {
+//             System.out.println("n is the prime number");
+//         } else {
+//             for (int i = 2; i < n - 1; i++) {
+//                 if (n % 1 == 0) {
+//                     isprime = false;
+//                 }
+//             }
+//             if (isprime == true) {
+//                 System.out.println("n is a prime number");
+//             } else {
+//                 System.out.println("n is not a prime number");
+//             }
+//         }
+//-------------------------------------------------------------------------------------------------------------
+// q2 write a program that reads a set of intgers and then print sum of event and add intger
+        // int even = 0;
+        // int odd = 0;
+        // while (true) {
+        //     System.out.print("enter a number: ");
+        //     int number = sc.nextInt();
+        //     if (number % 2 == 0) {
+        //         System.out.println("it is a even number");
+        //         System.out.println(even += number);
+        //     } else if (!(number % 2 == 0)) {
+        //         System.out.println("it is a odd number");
+        //         System.out.println(odd += number);
+        //     } else {
+        //         break;
+        //     }
+        // }
+//---------------------------------------------------------------------------------------------------------
+//q3. write a program to find a factorial of any number entered by the user 
+        // System.out.print("enter a number: ");
+        // int n = sc.nextInt();
+        // double factorial = 1;
+        // for (int i = n; i > 1; i--) {
+        //     // System.out.println(i);
+        //     factorial *= i;
+        // }
+        // System.out.println("the factorial of number is : " + factorial);
+//-----------------------------------------------------------------------------------------------------------
+// q4. write a program to print the multiplication table of a number N, enterd by the user 
+        // System.out.print("enter a number of which you want a table: ");
+        // int n = sc.nextInt();
 
-                }
-            }
-            if (isprime == true) {
-                System.out.println("n is a prime number");
-            } else {
-                System.out.println("n is not a prime number");
-            }
+        // for (int i = 1; i <= 10; i++) {
+        //     // System.out.println(i);
+        //     int table = i * n;
+        //     System.out.println(n + " * " + i + " = " + table);
+        // }
 
-        }
+
 
     }
 }
