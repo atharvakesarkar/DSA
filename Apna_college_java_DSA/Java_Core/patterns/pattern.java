3989f9b9b272d668476519678750d7987da0fafc
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-03-01 00:18:21 +0530

    Add files via upload

---
+++ Apna_college_java_DSA/Java_Core/patterns/pattern.java
@@ -0,0 +1,20 @@
+
+public class pattern {
+
+    public static void main(String args[]) {
+
+        // star pattern triangle....
+        // * 
+        // **
+        // ***        
+        // ****
+        for (int i = 1; i <= 4; i++) {
+            // System.out.printl`n("*");
+            for (int j = 1; j < i; j++) {
+                System.out.println("*");
+            }
+
+        }
+
+    }
+}
