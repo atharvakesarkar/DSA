9492ec9846ed847b5558d2bf87d37648ce96268e
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-03-04 20:30:39 +0530

    added the code about patterns

--- Apna_college_java_DSA/Java_Core/patterns/pattern.java
+++ Apna_college_java_DSA/Java_Core/patterns/pattern.java
@@ -8,11 +8,26 @@ public class pattern {
         // **
         // ***        
         // ****
-        for (int i = 1; i <= 4; i++) {
-            // System.out.printl`n("*");
-            for (int j = 1; j < i; j++) {
-                System.out.println("*");
+        // for (int i = 1; i <= 4; i++) {
+        //     // System.out.println("*");
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+//----------------------------------------------------------------------------------------------------------------
+        // print inverted-star pattern
+        // **** 
+        // *** 
+        // ** 
+        // *
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i + 1); j++) {
+                System.out.print("*");
             }
+            System.out.println();
 
         }
 
