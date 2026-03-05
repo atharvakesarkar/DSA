8a5f2137a0199537ba5fd88c7bee9608e35a8873
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-03-05 21:54:33 +0530

    Update pattern.java

--- Apna_college_java_DSA/Java_Core/patterns/pattern.java
+++ Apna_college_java_DSA/Java_Core/patterns/pattern.java
@@ -21,14 +21,34 @@ public class pattern {
         // *** 
         // ** 
         // *
+        // int n = 4;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= (n - i + 1); j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+//----------------------------------------------------------------------------------------------------------------
+        // print half pyramid pattern
+        // int n = 4;
+        // for (int line = 1; line <= n; line++) {
+        //     for (int number = 1; number <= line; number++) {
+        //         System.out.print(number);
+        //     }
+        //     System.out.println();
+        // }
+//----------------------------------------------------------------------------------------------------------------
+        // print character pattern
         int n = 4;
+        char ch = 'A';
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= (n - i + 1); j++) {
-                System.out.print("*");
+        for (int line = 1; line <= n; line++) {
+            for (int chars = 1; chars <= line; chars++) {
+
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
-
         }
 
     }
