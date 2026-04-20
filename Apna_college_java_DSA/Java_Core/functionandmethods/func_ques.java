b458787b4db8a0259f5328796a26bcd2b836f286
Atharva Kesarkar <206986641+atharvakesarkar@users.noreply.github.com>
2026-04-20 16:31:07 +0530

    Add files via upload

---
+++ Apna_college_java_DSA/Java_Core/functionandmethods/func_ques.java
@@ -0,0 +1,15 @@
+public  class func_ques{
+
+    public static int isAvg(int num1 , int num2 , int num3){
+        int result = (num1 + num2 + num3) / 3;
+        return result;
+    }
+
+    public static void main(String args[]){
+
+        System.out.println(isAvg(10, 10, 10));
+
+
+    }
+
+}
